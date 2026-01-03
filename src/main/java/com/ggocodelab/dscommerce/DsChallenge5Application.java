9eeb45cf38799aadec9f424bb24c006412a0c85f
ggocodelab <brgillian@gmail.com>
2026-01-03 20:27:17 -0300

    chore: initial spring boot project setup

---
+++ src/main/java/com/ggocodelab/dscommerce/DsChallenge5Application.java
@@ -0,0 +1,13 @@
+package com.ggocodelab.dscommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsChallenge5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsChallenge5Application.class, args);
+	}
+
+}

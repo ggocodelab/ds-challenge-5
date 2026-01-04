a8a52273a8e06868ab56494470cf7fb6db9aee7b
ggocodelab <brgillian@gmail.com>
2026-01-03 21:12:24 -0300

    feat(order): add order entity

---
+++ src/main/java/com/ggocodelab/dscommerce/entities/Order.java
@@ -0,0 +1,73 @@
+package com.ggocodelab.dscommerce.entities;
+
+import java.time.Instant;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order")
+public class Order {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private Instant momento;
+	
+	private OrderStatus status;
+	
+	public Order() {
+	}
+
+	public Order(Long id, Instant momento, OrderStatus status) {
+		this.id = id;
+		this.momento = momento;
+		this.status = status;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getMomento() {
+		return momento;
+	}
+
+	public void setMomento(Instant momento) {
+		this.momento = momento;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		return Objects.equals(id, other.id);
+	}
+}

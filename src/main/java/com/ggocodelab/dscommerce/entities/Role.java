8311e69b245d1656a78f19f05ff97be6b97d2be8
ggocodelab <brgillian@gmail.com>
2026-01-07 18:12:01 -0300

    chore(security): implement granted authority in role

--- src/main/java/com/ggocodelab/dscommerce/entities/Role.java
+++ src/main/java/com/ggocodelab/dscommerce/entities/Role.java
@@ -2,16 +2,19 @@ package com.ggocodelab.dscommerce.entities;
 
 import java.util.Objects;
 
+import org.springframework.security.core.GrantedAuthority;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+@SuppressWarnings("serial")
 @Entity
 @Table(name = "tb_role")
-public class Role {
-	
+public class Role implements GrantedAuthority{
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -33,11 +36,12 @@ public class Role {
 	public void setId(Long id) {
 		this.id = id;
 	}
-
+	
+	@Override
 	public String getAuthority() {
 		return authority;
 	}
-
+    
 	public void setAuthority(String authority) {
 		this.authority = authority;
 	}

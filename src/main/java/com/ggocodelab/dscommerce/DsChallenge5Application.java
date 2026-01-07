20cf7550bd7ebc4b7f90532a27a1565f9445a617
ggocodelab <brgillian@gmail.com>
2026-01-07 15:05:24 -0300

    chore(security): add password encryption test

--- src/main/java/com/ggocodelab/dscommerce/DsChallenge5Application.java
+++ src/main/java/com/ggocodelab/dscommerce/DsChallenge5Application.java
@@ -1,13 +1,27 @@
 package com.ggocodelab.dscommerce;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DsChallenge5Application {
+public class DsChallenge5Application implements CommandLineRunner{
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DsChallenge5Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("ENCODE = " + passwordEncoder.encode("123456"));
+		
+		boolean result = passwordEncoder
+				.matches("1234567", "$2a$10$N7SkKCa3r17ga.i.dF9iy.BFUBL2n3b6Z1CWSZWi/qy7ABq/E6VpO");
+		System.out.println("RESULTADO = " + result);
+	}
 }
--- src/main/java/com/ggocodelab/dscommerce/config/SecurityConfig.java
+++ src/main/java/com/ggocodelab/dscommerce/config/SecurityConfig.java
@@ -3,13 +3,19 @@ package com.ggocodelab.dscommerce.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+	@Bean
+	PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 	
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf(csrf -> csrf.disable());
 		http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
 		return http.build();

7c1c10906adcf882cdad0b6d9876c2ca1aef0e83
ggocodelab <brgillian@gmail.com>
2026-01-15 00:37:21 -0300

    chore(security): configure oauth2 token generation

--- src/main/java/com/ggocodelab/dscommerce/config/AuthorizationServerConfig.java
+++ src/main/java/com/ggocodelab/dscommerce/config/AuthorizationServerConfig.java
@@ -5,6 +5,7 @@ import java.security.KeyPairGenerator;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.time.Duration;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -17,9 +18,28 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.OAuth2Token;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.authorization.InMemoryOAuth2AuthorizationConsentService;
+import org.springframework.security.oauth2.server.authorization.InMemoryOAuth2AuthorizationService;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsentService;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
+import org.springframework.security.oauth2.server.authorization.authentication.OAuth2ClientAuthenticationToken;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.oauth2.server.authorization.token.DelegatingOAuth2TokenGenerator;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.JwtGenerator;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2AccessTokenGenerator;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenGenerator;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.nimbusds.jose.jwk.JWKSet;
@@ -30,6 +50,12 @@ import com.nimbusds.jose.proc.SecurityContext;
 @Configuration
 @EnableWebSecurity
 public class AuthorizationServerConfig {
+	
+	@Value("${security.client-id}")
+	private String clientId;
+
+	@Value("${security.client-secret}")
+	private String clientSecret;
 	
 	@Value("${security.jwt.duration}")
 	private Integer jwtDurationSeconds;
@@ -47,10 +73,40 @@ public class AuthorizationServerConfig {
 		return http.build();
 	}
 	
-	// Faz a encriptacao da senha
+	//9 Persiste as autorizacoes emitidas
+	@Bean
+	OAuth2AuthorizationService authorizationService() {
+		return new InMemoryOAuth2AuthorizationService();
+	}
+	
+	// 10 Consentimentos (opcional no password grant)
+	@Bean
+	OAuth2AuthorizationConsentService oAuth2AuthorizationConsentService() {
+		return new InMemoryOAuth2AuthorizationConsentService();
+	}
+	
+	// 6.Faz a encriptacao da senha
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	//7. Define clientId, secret, scopes e grant
+	@Bean
+	RegisteredClientRepository registeredClientRepository() {
+		
+		RegisteredClient registeredClient = RegisteredClient
+			.withId(UUID.randomUUID().toString())
+			.clientId(clientId)
+			.clientSecret(passwordEncoder().encode(clientSecret))
+			.scope("read")
+			.scope("write")
+			.authorizationGrantType(new AuthorizationGrantType("password"))
+			.tokenSettings(tokenSettings())
+			.clientSettings(clientSettings())
+			.build();
+
+		return new InMemoryRegisteredClientRepository(registeredClient);
 	}
 	
 	// 5. Define duracao e formato do token
@@ -60,6 +116,44 @@ public class AuthorizationServerConfig {
 			.accessTokenFormat(OAuth2TokenFormat.SELF_CONTAINED)
 			.accessTokenTimeToLive(Duration.ofSeconds(jwtDurationSeconds))
 			.build();
+	}
+	
+	// 7. Complementa o registeredClientRepository()
+	@Bean
+	ClientSettings clientSettings() {
+		return ClientSettings.builder().build();
+	}
+	
+	//8. endpoints padrão do AS
+	@Bean
+	AuthorizationServerSettings authorizationServerSettings() {
+		return AuthorizationServerSettings.builder().build();
+	}
+	
+	//11 Orquestra a geracao do access token
+	@Bean
+	OAuth2TokenGenerator<? extends OAuth2Token> tokenGenerator() {
+		NimbusJwtEncoder jwtEncoder = new NimbusJwtEncoder(jwkSource());
+		JwtGenerator jwtGenerator = new JwtGenerator(jwtEncoder);
+		jwtGenerator.setJwtCustomizer(tokenCustomizer());
+		OAuth2AccessTokenGenerator accessTokenGenerator = new OAuth2AccessTokenGenerator();
+		return new DelegatingOAuth2TokenGenerator(jwtGenerator, accessTokenGenerator);
+	}
+	// 12 
+	@Bean
+	OAuth2TokenCustomizer<JwtEncodingContext> tokenCustomizer() {
+		return context -> {
+			OAuth2ClientAuthenticationToken principal = context.getPrincipal();
+			CustomUserAuthorities user = (CustomUserAuthorities) principal.getDetails();
+			List<String> authorities = user.getAuthorities().stream().map(x -> x.getAuthority()).toList();
+			if (context.getTokenType().getValue().equals("access_token")) {
+				// @formatter:off
+				context.getClaims()
+					.claim("authorities", authorities)
+					.claim("username", user.getUsername());
+				// @formatter:on
+			}
+		};
 	}
 	
 	//4.Permite validacao dos tokens emitidos
